bb98782160f56da1780f5a0b96c4a4b789044c79
240484 <240484@jc-21.jp>
2025-11-10 13:48:43 +0900

    SpawnManagerクラス作った

--- Source/SpawnManager.h
+++ Source/SpawnManager.h
@@ -1,12 +1,16 @@
 #pragma once
+#include "../Library/GameObject.h"
+#include "Object2D.h"
 
-class SpawnManager
+class SpawnManager : public GameObject
 {
 public:
-	SpawnManager();
-	~SpawnManager();
-	void Update();
-	void Draw();
+	SpawnManager() = default;
+	virtual ~SpawnManager() = default;
+	virtual void Update() override = 0;
+	virtual void Draw() override = 0;
+
+	// 各種登録・リスポーン管理に共通で使う関数
+	virtual void RegisterRespawn(VECTOR2 pos, int handle) = 0;
 private:
-	
 };
